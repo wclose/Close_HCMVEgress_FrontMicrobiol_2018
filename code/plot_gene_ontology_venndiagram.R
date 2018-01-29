9141a3564188c67c663d15005e8e3413bea6f56c
William L. Close <close.will@gmail.com>
2018-01-29 04:52:48 -0500

    Added figure panel on neutrophil degranulation
    Decided to include based on significance in GO analysis

--- code/plot_gene_ontology_venndiagram.R
+++ code/plot_gene_ontology_venndiagram.R
@@ -6,12 +6,14 @@ transcript_transport_6810 <- read_tsv("data/transcript_transport_6810.txt", col_
 transcript_vesicle_mediated_transport_16192 <- read_tsv("data/transcript_vesicle_mediated_transport_16192.txt", col_names = F)
 transcript_secretion_46903 <- read_tsv("data/transcript_secretion_46903.txt", col_names = F)
 transcript_intracellular_transport_46907 <- read_tsv("data/transcript_intracellular_transport_46907.txt", col_names = F)
+transcript_neutrophil_degranulation_43312 <- read_tsv("data/transcript_neutrophil_degranulation_43312.txt", col_names = F)
 
 # importing protein data
 protein_transport_6810 <- read_tsv("data/protein_transport_6810.txt", col_names = F)
 protein_vesicle_mediated_transport_16192 <- read_tsv("data/protein_vesicle_mediated_transport_16192.txt", col_names = F)
 protein_secretion_46903 <- read_tsv("data/protein_secretion_46903.txt", col_names = F)
 protein_intracellular_transport_46907 <- read_tsv("data/protein_intracellular_transport_46907.txt", col_names = F)
+protein_neutrophil_degranulation_43312 <- read_tsv("data/protein_neutrophil_degranulation_43312.txt", col_names = F)
 
 
 # making 'transport' venn diagram 
@@ -43,4 +45,12 @@ grid.draw(vp_secretion)
 vp_intracellular_transport <- venn.diagram(list(t=transcript_intracellular_transport_46907$X1, p=protein_intracellular_transport_46907$X1), 
                    fill = c("firebrick3", "cyan4"), alpha = 0.3, filename = NULL);
 grid.draw(vp_intracellular_transport)
+#dev.off()
+
+
+# neutrophil degranulation diagram
+#pdf(file="figures/vp_neutrophil_degranulation.pdf")
+vp_neutrophil_degranulation <- venn.diagram(list(t=transcript_neutrophil_degranulation_43312$X1, p=protein_neutrophil_degranulation_43312$X1), 
+                                           fill = c("firebrick3", "cyan4"), alpha = 0.3, filename = NULL);
+grid.draw(vp_neutrophil_degranulation)
 #dev.off()
